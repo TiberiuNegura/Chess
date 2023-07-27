6364181cef53cf5c4808bc311ae3b458720cafbb
tibyEu <115886042+tibyEu@users.noreply.github.com>
2023-07-27 15:31:20 +0300

    observer mock tests completed

--- ChessTests/ListenerTests.cpp
+++ ChessTests/ListenerTests.cpp
@@ -7,6 +7,14 @@
 
 using ::testing::_;
 
+
+void MakeMovesFrom(std::vector<std::pair<Position, Position>>& moves, Game& game)
+{
+	for (auto move : moves)
+		EXPECT_NO_THROW(game.MovePiece(move.first, move.second));
+	moves.clear();
+}
+
 class MockObserver : public IGameListener
 {
 public:
@@ -187,20 +195,11 @@ TEST_F(GameMockTests, NotifyObserverTieRequest)
 	game.MakeTieRequest();
 }
 
-
-void MakeMovesFrom(std::vector<std::pair<Position, Position>>& moves, Game& game)
-{
-	for (auto move : moves)
-		EXPECT_NO_THROW(game.MovePiece(move.first, move.second));
-	moves.clear();
-}
-
 TEST_F(GameMockTests, NotifyObserver)
 {
 	Game game;
-
-	//25
 	game.AddListener(mock);
+
 	EXPECT_CALL(*mock, OnMovePiece).Times(25);
 	EXPECT_CALL(*mock, OnCheck).Times(4);
 	EXPECT_CALL(*mock, OnPawnEvolve).Times(1);
@@ -211,27 +210,31 @@ TEST_F(GameMockTests, NotifyObserver)
 		{{6,3},{4,3}},{{1,4},{3,4}},{{7,4},{6,3}},{{0,3},{3,6}}, // black checks white
 	};
 	MakeMovesFrom(moves, game);
-	// do some bad moves
+
+	EXPECT_THROW(game.MovePiece({ 3,6 }, { 9,12 }), OutOfBoundsException);
+	EXPECT_THROW(game.MovePiece({ 7,7 }, { 6,6 }), CheckException);
+
 	moves = {
 		{{6,4},{5,4}},{{3,6},{5,4}},{{6,3},{5,4}},{{0,4},{1,4}},
 		{{7,3},{5,3}},{{0,1},{2,2}},{{6,0},{4,0}},{{2,2},{4,1}},
 		{{4,0},{3,0}},{{1,1},{2,1}},{{3,0},{2,1}},{{0,2},{2,0}},
 		{{2,1},{1,2}},{{1,4},{2,5}},{{1,2},{0,2}}
 	}; // pawn evolve
-	// evolve the pawn
+
 	MakeMovesFrom(moves, game);
 	game.EvolvePawn(EType::QUEEN);
 
 	moves = {
 		{{4,1},{5,3}},{{0,2},{2,2}} // white checks black
 	};
+
 	MakeMovesFrom(moves, game);
-	// bad moves
+	EXPECT_THROW(game.MovePiece({ 7,2 }, { 3,6 }), CheckException);
+
 	game.Restart();
+
 	moves = {
 		{{6,6},{4,6}},{{1,4},{2,4}},{{6,5},{5,5}},{{0,3},{4,7}} // black checkmate white
 	};
 	MakeMovesFrom(moves, game);
-	// a few other bad moves, game over
 }
-// TODO: combine exception handling (EXPECT_THROW(...)) and mock with EXPECT_CALL(...).Times(0)
